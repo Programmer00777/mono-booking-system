d1b33f488df3e34b9ecc170e756bc4dbbb8232f8
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2023-01-12 12:29:57 +0200

    attempts to cover the project with tests, but I faced a lack of time

--- src/main/java/com/mono/bookingsystem/ticketsystem/exception/NoTicketsAvailableException.java
+++ src/main/java/com/mono/bookingsystem/ticketsystem/exception/NoTicketsAvailableException.java
@@ -1,8 +1,6 @@
 package com.mono.bookingsystem.ticketsystem.exception;
 
 public class NoTicketsAvailableException extends RuntimeException {
-    public NoTicketsAvailableException() {
-    }
 
     public NoTicketsAvailableException(String message) {
         super(message);
